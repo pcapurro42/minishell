563cbe02f77b1d892780ba8000d3f1830a002189
Valentin Delafosse <vdelafos@c2r7p4.42nice.fr>
2023-03-05 18:32:40 +0100

    add waitpid

--- execution/build_cmd_struct.c
+++ execution/build_cmd_struct.c
@@ -6,7 +6,7 @@
 /*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:38:28 by vdelafos          #+#    #+#             */
-/*   Updated: 2023/03/05 18:18:36 by vdelafos         ###   ########.fr       */
+/*   Updated: 2023/03/05 18:26:37 by vdelafos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,11 @@ static void	ft_case_right_chevron(t_mini *minishell, t_cmd *cmd_struct, \
 static void	ft_case_dbleft_chevron(t_mini *minishell, t_cmd *cmd_struct, \
 	int i, t_nb *nb)
 {
-	int pid;
-
 	if (cmd_struct->infile_fd >= 0)
 		close(cmd_struct->infile_fd);
-	pid = fork();
-	if (pid < 0)
-		ft_error();
-	if (pid == 0)
-	{
-		ft_here_doc(cmd_struct, \
-			minishell->cmd_lst[i][nb->j + 1]);
-		exit(0);
-	}
-	else
-		nb->j += 2;
+	ft_here_doc(cmd_struct, \
+		minishell->cmd_lst[i][nb->j + 1]);
+	nb->j += 2;
 }
 
 static void	ft_case_dright_chevron(t_mini *minishell, t_cmd *cmd_struct, \
--- execution/file.c
+++ execution/file.c
@@ -6,7 +6,7 @@
 /*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 10:59:38 by vdelafos          #+#    #+#             */
-/*   Updated: 2023/03/02 11:00:20 by vdelafos         ###   ########.fr       */
+/*   Updated: 2023/03/05 18:31:30 by vdelafos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,34 @@
 
 void	ft_here_doc(t_cmd *cmd_struct, char *limiter)
 {
+	int		pid;
 	char	*gnl;
 
 	cmd_struct->infile_fd = open("minishell_here_doc.tmp", \
 	O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (cmd_struct->infile_fd < 0)
 		return ;
-	write(0, "> ", 2);
-	gnl = get_next_line(0);
-	ft_check_malloc(gnl);
-	while (ft_strncmp(gnl, limiter, ft_strlen(gnl)) != '\n')
+	pid = fork();
+	if (pid < 0)
+		ft_error();
+	if (pid == 0)
 	{
-		write(cmd_struct->infile_fd, gnl, ft_strlen(gnl));
-		free(gnl);
 		write(0, "> ", 2);
 		gnl = get_next_line(0);
 		ft_check_malloc(gnl);
+		while (ft_strncmp(gnl, limiter, ft_strlen(gnl)) != '\n')
+		{
+			write(cmd_struct->infile_fd, gnl, ft_strlen(gnl));
+			free(gnl);
+			write(0, "> ", 2);
+			gnl = get_next_line(0);
+			ft_check_malloc(gnl);
+		}
+		free(gnl);
+		close(cmd_struct->infile_fd);
+		exit(0);
 	}
-	free(gnl);
+	waitpid(pid, NULL, 0);
 	close(cmd_struct->infile_fd);
 	cmd_struct->infile_fd = open("minishell_here_doc.tmp", O_RDONLY);
 	return ;
