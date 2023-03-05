530bf3cad735ae0d251f4960426a4c0eac7bc26a
Valentin Delafosse <vdelafos@c2r7p4.42nice.fr>
2023-03-05 16:47:16 +0100

    modif exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 14:55:57 by vdelafos          #+#    #+#             */
-/*   Updated: 2023/03/05 16:46:10 by vdelafos         ###   ########.fr       */
+/*   Updated: 2023/03/05 16:47:00 by vdelafos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_exit_builtins(t_cmd *cmd_struct)
 	char	*temp2;
 
 	ft_putstr_fd("exit\n", 2);
-	if (!(cmd_struct->cmd_arg[1]))
+	if (!(cmd_struct) || !(cmd_struct->cmd_arg[1]))
 		exit(0);
 	else if (ft_isnum(cmd_struct->cmd_arg[1]))
 		exit(ft_atoi(cmd_struct->cmd_arg[1]) % 256);
