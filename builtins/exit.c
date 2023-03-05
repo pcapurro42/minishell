2ea345fd5be667df996708bb5b7625ae40988ea1
Valentin Delafosse <vdelafos@c2r7p4.42nice.fr>
2023-03-05 16:46:42 +0100

    add putstr exit for exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 14:55:57 by vdelafos          #+#    #+#             */
-/*   Updated: 2023/03/05 15:41:04 by vdelafos         ###   ########.fr       */
+/*   Updated: 2023/03/05 16:46:10 by vdelafos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_exit_builtins(t_cmd *cmd_struct)
 	char	*temp;
 	char	*temp2;
 
+	ft_putstr_fd("exit\n", 2);
 	if (!(cmd_struct->cmd_arg[1]))
 		exit(0);
 	else if (ft_isnum(cmd_struct->cmd_arg[1]))
