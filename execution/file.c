603d94894d69ef8e10caf15f093b7a44326be65d
Valentin Delafosse <vdelafos@c2r6p3.42nice.fr>
2023-03-06 18:31:04 +0100

    file

---
+++ execution/file.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 10:59:38 by vdelafos          #+#    #+#             */
+/*   Updated: 2023/03/06 18:29:22 by vdelafos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	ft_here_doc(t_cmd *cmd_struct, char *limiter)
+{
+	int		pid;
+	char	*gnl;
+
+	pid = fork();
+	if (pid < 0)
+		ft_error();
+	if (pid == 0)
+	{
+		cmd_struct->infile_fd = open("minishell_here_doc.tmp", \
+		O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (cmd_struct->infile_fd < 0)
+			ft_error();
+		write(0, "> ", 2);
+		gnl = get_next_line(0);
+		if (!gnl)
+		{
+			write(0, "\n", 1);
+			exit(0);
+		}
+		while (ft_strncmp(gnl, limiter, ft_strlen(gnl)) != '\n')
+		{
+			write(cmd_struct->infile_fd, gnl, ft_strlen(gnl));
+			free(gnl);
+			write(0, "> ", 2);
+			gnl = get_next_line(0);
+			if (!gnl)
+			{
+				write(0, "\n", 1);
+				exit(0);
+			}
+		}
+		free(gnl);
+		close(cmd_struct->infile_fd);
+		exit(0);
+	}
+	here_doc_pid = pid;
+	waitpid(pid, NULL, 0);
+	here_doc_pid = -1;
+	cmd_struct->infile_fd = open("minishell_here_doc.tmp", O_RDONLY);
+	return ;
+}
+
+void	file_not_open(t_cmd *cmd_struct, char *file_name)
+{
+	ft_putstr_fd("minishell: ", 2);
+	perror(file_name);
+	cmd_struct->infile_fd = open("minishell_here_doc.tmp", \
+	O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (cmd_struct->infile_fd < 0)
+		ft_error();
+	close(cmd_struct->infile_fd);
+	cmd_struct->infile_fd = open("minishell_here_doc.tmp", O_RDONLY);
+	return ;
+}
