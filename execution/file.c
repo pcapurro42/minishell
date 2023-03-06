b15ce6ef2bb3a2ea65d15d98b86118ba57090bc9
Valentin Delafosse <vdelafos@c2r6p3.42nice.fr>
2023-03-06 18:37:08 +0100

    remove \n

--- execution/file.c
+++ execution/file.c
@@ -6,7 +6,7 @@
 /*   By: vdelafos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 10:59:38 by vdelafos          #+#    #+#             */
-/*   Updated: 2023/03/06 18:29:22 by vdelafos         ###   ########.fr       */
+/*   Updated: 2023/03/06 18:36:24 by vdelafos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,18 @@ void	ft_here_doc(t_cmd *cmd_struct, char *limiter)
 		O_CREAT | O_RDWR | O_TRUNC, 0644);
 		if (cmd_struct->infile_fd < 0)
 			ft_error();
-		write(0, "> ", 2);
+		write(1, "> ", 2);
 		gnl = get_next_line(0);
 		if (!gnl)
-		{
-			write(0, "\n", 1);
 			exit(0);
-		}
 		while (ft_strncmp(gnl, limiter, ft_strlen(gnl)) != '\n')
 		{
 			write(cmd_struct->infile_fd, gnl, ft_strlen(gnl));
 			free(gnl);
-			write(0, "> ", 2);
+			write(1, "> ", 2);
 			gnl = get_next_line(0);
 			if (!gnl)
-			{
-				write(0, "\n", 1);
 				exit(0);
-			}
 		}
 		free(gnl);
 		close(cmd_struct->infile_fd);
