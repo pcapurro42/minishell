77551ed642beca89abf433d1e4d0091c98910afc
Patrick Capurro <pcapurro@c2r1p1.42nice.fr>
2023-03-31 08:28:04 +0200

    forgot one character in print for ctrl \ in cat

--- minishell.c
+++ minishell.c
@@ -75,7 +75,7 @@ void	ft_print_shell(int signal)
 	if (signal == SIGQUIT)
 	{
 		if (g_global->g_pid >= 0)
-			ft_putstr_fd("Quit: 3\n", 1);
+			ft_putstr_fd("^\\Quit: 3\n", 1);
 		rl_redisplay();
 	}
 }
